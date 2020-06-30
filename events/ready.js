0d4bcd5a565457fc1e095102f07fe1bba6b8a271
DxxxxY <introsdream@gmail.com>
2020-06-30 01:18:34 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -14,6 +14,6 @@ module.exports = async(client) => {
         console.log('Mongoose connection disconnected')
     })
     console.log(`Logged in as ${client.user.tag}!`)
-    client.user.setActivity(`over ${client.users.cache.size - 1} users | v1.0.1`, { type: "WATCHING" })
-    setInterval(() => { client.user.setActivity(`over ${client.users.cache.size-1} users | v1.0.1`, { type: "WATCHING" }) }, 60000)
+    client.user.setActivity(`over ${client.users.cache.size - 1} users | v1.1`, { type: "WATCHING" })
+    setInterval(() => { client.user.setActivity(`over ${client.users.cache.size-1} users | v1.1`, { type: "WATCHING" }) }, 60000)
 }
